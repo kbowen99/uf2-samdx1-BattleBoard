37e11337afb4e2d39c7501a33cd1cd7dda57a936
kbowen99 <kurtisbowen99@gmail.com>
2021-03-09 04:41:37 -0600

    Multiple Dotstar Test certain boards might have multiple dotstars... Like a particular robot indicator light

--- boards/FightLight/board_config.h
+++ boards/FightLight/board_config.h
@@ -12,11 +12,8 @@
 #define USB_VID 0x239A
 #define USB_PID 0x001E
 
-//#define LED_PIN PIN_PA10
-//#define LED_TX_PIN PIN_PA27
-//#define LED_RX_PIN PIN_PB03
-
 #define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
 #define BOARD_RGBLED_DATA_PIN             PIN_PA00
+#define BOARD_RGBLED_COUNT                3
 
 #endif
